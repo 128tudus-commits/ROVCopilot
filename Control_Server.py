730195479a7eeb1a2b858b195b022675531239fc
128tudus-commits <128tudus@gmail.com>
2026-06-02 17:13:47 +0200

    Change Flask app host to '0.0.0.0'

--- Control_Server.py
+++ Control_Server.py
@@ -676,4 +676,4 @@ if __name__ == '__main__':
     print("╚════════════════════════════════════════════════════╝\n")
     
     # Run Flask app
-    app.run(debug=True, host='localhost', port=5000, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
