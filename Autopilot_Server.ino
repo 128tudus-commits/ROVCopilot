0b99b366c4e4a8befeba0a25408adc3d31be801e
128tudus-commits <128tudus@gmail.com>
2026-06-21 14:37:32 +0200

    Refactor motor control logic and improve readability

--- Autopilot_Server.ino
+++ Autopilot_Server.ino
@@ -1,82 +1,83 @@
-// Arduino code
-float M1;
-float M2;
-float M3;
-float M4;
-void setup(){
-Serial.begin(115200);
-pinMode(2, OUTPUT);
-pinMode(3, OUTPUT);
-pinMode(4, OUTPUT);
-pinMode(5, OUTPUT);
-pinMode(6, OUTPUT);
-pinMode(7, OUTPUT);
-pinMode(8, OUTPUT);
-pinMode(9, OUTPUT);
-}
+const int E1A = 2;
+const int E1B = 3;
+const int E2A = 4;
+const int E2B = 5;
+const int E3A = 6;
+const int E3B = 7;
+const int E4A = 8;
+const int E4B = 9;
 
-void loop(){
+const float threshold = 0.2; 
 
+unsigned long lastCommunicationTime = 0;
+const unsigned long timeoutDuration = 1000;
 
-M1 = Serial.parseFloat();
-M2 = Serial.parseFloat();
-M3 = Serial.parseFloat();
-M4 = Serial.parseFloat();
-
-
-if (M1 > 0.5){
- digitalWrite(2, HIGH);
- digitalWrite(3, LOW);
-}
-else if (M1 < 0.5){
- digitalWrite(3, HIGH);
- digitalWrite(2, LOW);
-}
-else{
-digitalWrite(2, LOW);
-digitalWrite(3, LOW);
-}
-if (M2 > 0.5){
- digitalWrite(4, HIGH);
- digitalWrite(5, LOW);
-}
-else if (M2 < 0.5){
- digitalWrite(5, HIGH);
- digitalWrite(4, LOW);
-}
-else{
-digitalWrite(4, LOW);
-digitalWrite(5, LOW);
-}
-
-
-if (M3 > 0.5){
- digitalWrite(6, HIGH);
- digitalWrite(7, LOW);
-}
-else if (M3 < 0.5){
- digitalWrite(7, HIGH);
- digitalWrite(6, LOW);
-}
-else{
-digitalWrite(6, LOW);
-digitalWrite(7, LOW);
-}
-
-
-if (M4 > 0.5){
- digitalWrite(8, HIGH);
- digitalWrite(9, LOW);
-}
-else if (M4 < 0.5){
- digitalWrite(9, HIGH);
- digitalWrite(8, LOW);
-}
-else{
-digitalWrite(8, LOW);
-digitalWrite(9, LOW);
-}
-}
-
-
+void setup() {
+  Serial.begin(115200); 
   
+  pinMode(E1A, OUTPUT);
+  pinMode(E1B, OUTPUT);
+  pinMode(E2A, OUTPUT);
+  pinMode(E2B, OUTPUT);
+  pinMode(E3A, OUTPUT);
+  pinMode(E3B, OUTPUT);
+  pinMode(E4A, OUTPUT);
+  pinMode(E4B, OUTPUT);
+  
+  lastCommunicationTime = millis();
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    data.trim();
+    
+    int firstSemi = data.indexOf(';');
+    int secondSemi = data.indexOf(';', firstSemi + 1);
+    int thirdSemi = data.indexOf(';', secondSemi + 1);
+    
+    if (firstSemi != -1 && secondSemi != -1 && thirdSemi != -1) {
+      float val1 = data.substring(0, firstSemi).toFloat();
+      float val2 = data.substring(firstSemi + 1, secondSemi).toFloat();
+      float val3 = data.substring(secondSemi + 1, thirdSemi).toFloat();
+      float val4 = data.substring(thirdSemi + 1).toFloat();
+      
+      controlMotor(E1A, E1B, val1);
+      controlMotor(E2A, E2B, val2);
+      controlMotor(E3A, E3B, val3);
+      controlMotor(E4A, E4B, val4);
+      
+      lastCommunicationTime = millis();
+    }
+  }
+
+  if (millis() - lastCommunicationTime > timeoutDuration) {
+    stopAllMotors();
+  }
+}
+
+void controlMotor(int pinA, int pinB, float value) {
+  if (value > threshold) {
+    digitalWrite(pinA, HIGH);
+    digitalWrite(pinB, LOW);
+  } 
+  else if (value < -threshold) {
+    digitalWrite(pinA, LOW);
+    digitalWrite(pinB, HIGH);
+  } 
+  else {
+    digitalWrite(pinA, LOW);
+    digitalWrite(pinB, LOW);
+  }
+}
+
+void stopAllMotors() {
+  digitalWrite(E1A, LOW);
+  digitalWrite(E1B, LOW);
+  digitalWrite(E2A, LOW);
+  digitalWrite(E2B, LOW);
+  digitalWrite(E3A, LOW);
+  digitalWrite(E3B, LOW);
+  digitalWrite(E4A, LOW);
+  digitalWrite(E4B, LOW);
+}
