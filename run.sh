622ce741588bd06c52cde8a88a371c46498fc007
128tudus-commits <128tudus@gmail.com>
2026-06-30 21:40:16 +0200

    Create run.sh to set up and run Control Server

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/Documents
+
+sudo apt install -y ustreamer
+python3 -m pip install --upgrade pip
+python3 -m pip install pyserial flask
+
+python3 Control_Server.py & ustreamer --device=/dev/video1 --host=0.0.0.0 --port=8080 --desired-fps=30
