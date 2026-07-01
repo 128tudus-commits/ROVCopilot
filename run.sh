f5d8fe0adbb9d57565a7dfcca7667f0e2e391be4
128tudus-commits <128tudus@gmail.com>
2026-07-01 09:51:10 +0200

    Update run.sh to install packages in one command

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y ustreamer
-python3 -m pip install --upgrade pip
-python3 -m pip install pyserial flask
+sudo apt update
+sudo apt install -y ustreamer python3-flask python3-serial
 
 python3 Control_Server.py & ustreamer --device=/dev/video1 --host=0.0.0.0 --port=8080 --desired-fps=30
